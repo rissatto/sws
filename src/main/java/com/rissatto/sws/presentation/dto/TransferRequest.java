282b34f67945eb902ae49e6fd3c8230f6d125c57
Elison Rissatto <elison@rissatto.com>
2025-04-28 14:44:16 -0300

    Fix transfer operation

--- src/main/java/com/rissatto/sws/presentation/dto/TransferRequest.java
+++ src/main/java/com/rissatto/sws/presentation/dto/TransferRequest.java
@@ -1,7 +1,9 @@
 package com.rissatto.sws.presentation.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public record TransferRequest(UUID targetWalletId, BigDecimal amount) {
+public record TransferRequest(@JsonProperty("targetWalletId") UUID targetWalletId, BigDecimal amount) {
 }
